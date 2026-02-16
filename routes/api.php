5312108673e724ecb9d67889b5bd5e0313372649
Nicolas Gonzalez <ng33446@gmail.com>
2026-02-15 22:18:33 -0500

    feat: exponer ruta principal para los endpoins con el prefijo /api/v1/

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
+});
+
+Route::prefix('v1/')->group(function () {
+	Route::resource('authors', AuthorController::class);
+	Route::resource('books', BookController::class);
 });
